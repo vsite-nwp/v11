23af3e5d6ab0252e7bce4d970136af61fd505531
zugcic <stzugcic@vsite.hr>
2016-01-16 19:54:52 +0100

    Sve gorovo.

--- v11View.cpp
+++ v11View.cpp
@@ -33,7 +33,7 @@ END_MESSAGE_MAP()
 
 // Cv11View construction/destruction
 
-Cv11View::Cv11View() : shape(0), shape_t(0), color(0)
+Cv11View::Cv11View() : shape(0), shape_t(0), color(0),color_t(0)
 {}
 
 Cv11View::~Cv11View()
