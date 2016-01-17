51fd72e997689ea065bf2a94a5a19e37e10871cf
vanjs83 <tihomir.vanjurek@gmail.com>
2016-01-17 01:16:31 +0100

    change code!!

--- v11View.cpp
+++ v11View.cpp
@@ -36,8 +36,8 @@ END_MESSAGE_MAP()
 Cv11View::Cv11View() {
 	shape =0 ;
 	color = 0;
-	tempshape = -1;
-	tempcolor = -1;
+	tempshape = 0;
+	tempcolor = 0;
 }
 
 Cv11View::~Cv11View()
@@ -185,26 +185,23 @@ LRESULT Cv11View::OnHighlightRibbonListItem(WPARAM wp, LPARAM lp)
 	int index = (int)wp;
 	CMFCRibbonBaseElement* pElem = (CMFCRibbonBaseElement*)lp;
 	UINT id = pElem->GetID();
-	return 0;
+	
 	if (id == ID_SHAPE)
-	{
-		if (index >= 0)
+	{ 
 			shape = index;
-		else if (index == -1) 
-		{
-			shape = tempshape;
-			Invalidate();
-		}
 	}
 	if (id == ID_COLOR)
 	{
 		CMFCRibbonColorButton* pColor = (CMFCRibbonColorButton*)pElem;
 		color = pColor->GetHighlightedColor();
+	}
 		if (index == -1) 
 		{
+			shape = tempshape;
 			color = tempcolor;
-			Invalidate();
+			
 		}
 
-	}
+		Invalidate();
+	return 0;
 }
