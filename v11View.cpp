36feb5a5ced73145580cb18bc2895ba63f108536
Renato.Katalenic <druidhr@gmail.com>
2017-01-16 19:00:49 +0100

    Inicijalizirani member currentshape i currentcolor.

--- v11View.cpp
+++ v11View.cpp
@@ -33,7 +33,7 @@ END_MESSAGE_MAP()
 
 // Cv11View construction/destruction
 
-Cv11View::Cv11View(): shape(0), color(0) {}
+Cv11View::Cv11View(): shape(0), color(0), currentShape(0), currentColor(0) {}
 
 Cv11View::~Cv11View()
 {
